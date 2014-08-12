7b7a749a5b8b33f570a07273cf4a471379c828f2
rotcivegaf <victorfage@gmail.com>
2014-08-12 17:49:36 -0300

    now can see the keys

--- menu.js
+++ menu.js
@@ -2,17 +2,23 @@
 'use strict';
     var menu_state = {
     create: function() {
+        var xControl = 20, yControl = game.world.height-80;
+        var xCenter = game.world.width/2, yCenter = game.world.height/2;
         var startButtom = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         startButtom.onDown.add(this.start, this);
-  
+        
         var style = { font: '30px Arial', fill: '#ffffff' };
-        var x = game.world.width/2, y = game.world.height/2;
-
-        var text = this.add.text(x, y-50, 'Press Space to start', style);
+        var text = this.add.text(xCenter, yCenter-50, 'Press Space to start', style);
         text.anchor.setTo(0.5, 0.5);
 
+        var controlStyle = { font: '20px Arial', fill: '#0fa0ff' };
+        var heightStyle = 20;
+        var fireText = this.add.text(xControl, yControl, 'Fire: Space Bar', controlStyle);
+        var upText = this.add.text(xControl, yControl+heightStyle, 'Move up: Up arrow', controlStyle);
+        var downText = this.add.text(xControl, yControl+(heightStyle*2), 'Dove down: Down arrow', controlStyle);
+
         if (score > 0) {
-            var score_label = this.add.text(x, y+50, 'score: ' + score, style);
+            var score_label = this.add.text(xCenter, yCenter+50, 'score: ' + score, style);
             score_label.anchor.setTo(0.5, 0.5);
         }
     },
